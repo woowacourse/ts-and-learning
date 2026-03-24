045ad614d572ed00e1750fb4cf0cf4222670e2c5
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-24 20:54:19 +0900

    feat: exercise-04 문제 풀이 및 과정 기록

--- exercises/exercise-06/index.ts
+++ exercises/exercise-06/index.ts
@@ -28,45 +28,56 @@ Higher difficulty bonus exercise:
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+  type: 'user';
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
 }
 
 export type Person = User | Admin;
 
 export const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
-    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+  {
+    type: 'user',
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+  { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+  { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' },
 ];
 
 export function logPerson(person: Person) {
-    console.log(
-        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
-    );
+  console.log(
+    ` - ${person.name}, ${person.age}, ${
+      person.type === 'admin' ? person.role : person.occupation
+    }`,
+  );
 }
 
-export function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
-    return persons
-        .filter((person) => person.type === personType)
-        .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
-            return criteriaKeys.every((fieldName) => {
-                return person[fieldName] === criteria[fieldName];
-            });
-        });
+export function filterPersons(
+  persons: Person[],
+  personType: string,
+  criteria: unknown,
+): unknown[] {
+  return persons
+    .filter((person) => person.type === personType)
+    .filter((person) => {
+      let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+      return criteriaKeys.every((fieldName) => {
+        return person[fieldName] === criteria[fieldName];
+      });
+    });
 }
 
 export const usersOfAge23 = filterPersons(persons, 'user', { age: 23 });
