006618fa8000496fabcccdeb788f727355173fc8
김상윤 <ksy151020@gmail.com>
2026-03-25 20:50:25 +0900

    feat: - 11번 12번 문제 해결 - 학습로그 작성

--- exercises/exercise-12/declarations/stats/index.d.ts
+++ exercises/exercise-12/declarations/stats/index.d.ts
@@ -1,3 +1,10 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export function getMaxIndex<T>(input: Array<T>, comparator: (a:T,b:T)=>number): number | -1;
+    export function getMaxElement<T>(input: Array<T>, comparator: (a:T,b:T)=>number):T | null;
+    export function getMinIndex<T>(input: Array<T>, comparator: (a:T,b:T)=>number):number | -1;
+    export function getMinElement<T>(input: Array<T>, comparator:(a:T,b:T)=>number):T | null;
+    export function getMedianIndex<T>(input: Array<T>, comparator: (a:T,b:T)=>number):number | -1;
+    export function getMedianElement<T>(input: Array<T>, comparator: (a:T,b:T)=>number):T | null;
+    export function getAverageValue<T>(input: Array<T>, getValue:(item:T) => number):number | null;
+    
 }
