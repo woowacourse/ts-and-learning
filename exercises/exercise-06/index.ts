5f51aff04e7aa5cef553a07d90205a567339fde4
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-25 14:25:01 +0900

    feat: exercise-06 문제 풀이 및 과정 기록

--- exercises/exercise-06/index.ts
+++ exercises/exercise-06/index.ts
@@ -67,13 +67,24 @@ export function logPerson(person: Person) {
 
 export function filterPersons(
   persons: Person[],
+  personType: 'user',
+  criteria: Partial<Omit<User, 'type'>>,
+): User[];
+export function filterPersons(
+  persons: Person[],
+  personType: 'admin',
+  criteria: Partial<Omit<Admin, 'type'>>,
+): Admin[];
+export function filterPersons(
+  persons: Person[],
   personType: string,
-  criteria: unknown,
-): unknown[] {
+  criteria: Partial<Person>,
+): Person[] {
   return persons
     .filter((person) => person.type === personType)
     .filter((person) => {
       let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+
       return criteriaKeys.every((fieldName) => {
         return person[fieldName] === criteria[fieldName];
       });
