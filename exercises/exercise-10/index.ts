64bca2140c9cba25f26ed58085dd3c806f8713ff
김상윤 <ksy151020@gmail.com>
2026-03-25 18:04:11 +0900

    feat: - 9,10문제 풀이 - 나만의 claude code skills 제작 - 학습로그 3 작성 - 스터디 도구 로그 작성

--- exercises/exercise-10/index.ts
+++ exercises/exercise-10/index.ts
@@ -72,8 +72,16 @@ export type ApiResponse<T> = (
     }
 );
 
-export function promisify(arg: unknown): unknown {
-    return null;
+export function promisify<T>(arg:(callback: (response: ApiResponse<T>) => void) => void):() => Promise<T> {
+    return () => new Promise<T>((resolve, reject) => {
+        arg((response) => {
+            if (response.status === 'success') {
+                resolve(response.data);
+            } else {
+                reject(response.error);
+            }
+        })
+    });
 }
 
 const oldApi = {
