ccae8ae3a80f18d4b92496807c05a56b84c321ab
TH_97 <hood3392@gmail.com>
2026-03-26 14:52:39 +0900

    chore: reset exercise-10

--- exercises/exercise-10/index.ts
+++ exercises/exercise-10/index.ts
@@ -76,16 +76,8 @@ export type ApiResponse<T> =
       error: string;
     };
 
-export function promisify<T>(
-  arg: (callback: (response: ApiResponse<T>) => void) => void,
-): () => Promise<T> {
-  return () =>
-    new Promise((resolve, reject) => {
-      arg((response) => {
-        if (response.status === 'success') resolve(response.data);
-        if (response.status === 'error') reject(new Error(response.error));
-      });
-    });
+export function promisify(arg: unknown): unknown {
+  return null;
 }
 
 const oldApi = {
