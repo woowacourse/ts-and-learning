73bd8722d544c72234d4f1e8436e71cd91a9f07d
bel1c10ud <86952779+bel1c10ud@users.noreply.github.com>
2026-03-27 22:45:36 +0900

    [TS 와 학습법 튜닝하기] 클라우디 미션 제출합니다. (#26)
    * docs: 사전 질문 작성
    
    * feat: 1~4번 문제 해결
    
    * docs: 첫번째 학습 로그 작성
    
    * feat: 5~9번 문제 해결
    
    * docs: 두번째 학습 로그 작성
    
    * docs: 학습 도구 로그 작성
    
    * feat: 10~12번 문제 해결
    
    * docs: 세번째 학습 로그 작성
    
    * feat: 13번 문제 해결
    
    * docs: 네번째 학습 로그 작성
    
    * docs: 학습 도구 로그 갱신
    
    * feat: 14번 문제 미해결

--- exercises/exercise-12/declarations/stats/index.d.ts
+++ exercises/exercise-12/declarations/stats/index.d.ts
@@ -1,3 +1,23 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+  type Comapre<T> = (a: T, b: T) => number;
+  type Getter<T> = (a: T) => number;
+  type GetIndexFn = <T>(input: T[], comparator: Comapre<T>) => number;
+  type GetElementFn = <T>(input: T[], comparator: Comapre<T>) => T | null;
+  type GetValueFn = <T>(input: T[], getter: Getter<T>) => number | null;
+
+  export const getMaxIndex: GetIndexFn;
+  export const getMinIndex: GetIndexFn;
+  export const getMaxElement: GetElementFn;
+  export const getMinElement: GetElementFn;
+  export const getMedianIndex: GetIndexFn;
+  export const getMedianElement: GetElementFn;
+  export const getAverageValue: GetValueFn;
+
+  //   export function getMaxIndex<T>(input: T[], comparator: (a: T, b: T) => number): number;
+  //   export function getMaxElement<T>(input: T[], comparator: (a: T, b: T) => number): T | null;
+  //   export function getMinIndex<T>(input: T[], comparator: (a: T, b: T) => number): number;
+  //   export function getMinElement<T>(input: T[], comparator: (a: T, b: T) => number): T | null ;
+  //   export function getMedianIndex<T>(input: T[], comparator: (a: T, b: T) => number): number;
+  //   export function getMedianElement<T>(input: T[], comparator: (a: T, b: T) => number): T | null;
+  //   export function getAverageValue<T>(input: T[], getter: (a: T) => number): number | null;
 }
