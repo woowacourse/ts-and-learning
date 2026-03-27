fbbe7ce2fd982120c5ef8c36bf7017ecbb7db9ec
eunnn <86216821+DongEun02@users.noreply.github.com>
2026-03-27 19:45:55 +0900

    [TS 와 학습법 튜닝하기] 디움 미션 제출합니다. (#19)
    * docs: 기존 학습 방법 작성
    
    * feat: exercise 1번 구현
    
    * feat: exercise 2번 구현
    
    * feat: exercise 3번 구현
    
    * feat: exercise 4번 구현
    
    * feat: exercise 5번 구현
    
    * feat: exercise 6번 구현
    
    * docs: 학습 전략 및 평가 추가
    
    * docs: 스터디 로그 작성
    
    * feat: exercise 7번 구현
    
    * feat: exercise 8번 구현
    
    * feat: exercise 9번 구현
    
    * docs: 학습 로그 작성
    
    * docs: 학습 로그 작성
    
    * docs: 학습툴 로그 작성
    
    * feat: exercise 10번 구현
    
    * feat: exercise 11번 구현
    
    * feat: exercise 12번 구현
    
    * docs: 학습 로그 수정

--- exercises/exercise-11/declarations/str-utils/index.d.ts
+++ exercises/exercise-11/declarations/str-utils/index.d.ts
@@ -1,4 +1,11 @@
+type Func = (value: string) => string;
+
 declare module 'str-utils' {
-    // export const ...
-    // export function ...
+  // export const ...
+  // export function ...
+  export const strReverse: Func;
+  export const strToLower: Func;
+  export const strToUpper: Func;
+  export const strRandomize: Func;
+  export const strInvertCase: Func;
 }
--- exercises/exercise-12/declarations/stats/index.d.ts
+++ exercises/exercise-12/declarations/stats/index.d.ts
@@ -1,3 +1,16 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+  type Comparator<T> = (a: T, b: T) => number;
+  type GetValue<T> = (a: T) => number;
+
+  type IndexFunc = <T>(input: T[], comparator: Comparator<T>) => number;
+  type ElementFunc = <T>(input: T[], comparator: Comparator<T>) => T | null;
+  type ValueFunc = <T>(input: T[], getValue: GetValue<T>) => number | null;
+
+  export const getMaxIndex: IndexFunc;
+  export const getMaxElement: ElementFunc;
+  export const getMinIndex: IndexFunc;
+  export const getMinElement: ElementFunc;
+  export const getMedianIndex: IndexFunc;
+  export const getMedianElement: ElementFunc;
+  export const getAverageValue: ValueFunc;
 }
