48be8912774e5f1bd6961fb07b8e5921085b682b
송아영 <141705278+gustn99@users.noreply.github.com>
2026-03-30 13:36:01 +0900

    [TS 와 학습법 튜닝하기] 아지 미션 제출합니다. (#15)
    * docs: 학습 로그 업데이트
    
    * fix: exercise-01 해결
    
    * fix: exercise-02 해결
    
    * fix: exercise-03 해결
    
    * chore: typescript 버전 다운그레이드
    
    * fix: exercise-04 해결
    
    * fix: exercise-05 해결
    
    * refactor: exercise-05 보너스 문제 해결
    
    * fix: exercise-06 해결
    
    * docs: 학습 도구 로그 작성
    
    * docs: 1차 학습 로그 작성
    
    * fix: exercise-07 해결
    
    * fix: exercise-08 해결
    
    * docs: 2차 학습 로그 작성
    
    * fix: exercise-09 해결
    
    * fix: exercise-10 해결
    
    * docs: 3차 학습 로그 작성
    
    * docs: 4차 학습 로그 수행 전, 수정 사항 작성
    
    * refactor: exercise-06 보너스 문제 해결
    
    * docs: 4차 학습 로그 중간 과정 업데이트
    
    * refactor: exercise-10 보너스 문제 해결
    
    * refactor: exercise-10 보너스 문제 mapped 타입으로 리팩토링
    
    * docs: 4차 학습 로그 업데이트

--- exercises/exercise-01/index.ts
+++ exercises/exercise-01/index.ts
@@ -76,26 +76,30 @@ Exercise:
 
 */
 
-export type User = unknown;
+export type User = {
+	name: string;
+	age: number;
+	occupation: string;
+};
 
-export const users: unknown[] = [
-  {
-    name: "Max Mustermann",
-    age: 25,
-    occupation: "Chimney sweep",
-  },
-  {
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
+export const users: User[] = [
+	{
+		name: 'Max Mustermann',
+		age: 25,
+		occupation: 'Chimney sweep',
+	},
+	{
+		name: 'Kate Müller',
+		age: 23,
+		occupation: 'Astronaut',
+	},
 ];
 
-export function logPerson(user: unknown) {
-  console.log(` - ${user.name}, ${user.age}`);
+export function logPerson(user: User) {
+	console.log(` - ${user.name}, ${user.age}`);
 }
 
-console.log("Users:");
+console.log('Users:');
 users.forEach(logPerson);
 
 // In case you are stuck:
--- exercises/exercise-10/index.ts
+++ exercises/exercise-10/index.ts
@@ -36,110 +36,135 @@ Higher difficulty bonus exercise:
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+	type: 'user';
+	name: string;
+	age: number;
+	occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+	type: 'admin';
+	name: string;
+	age: number;
+	role: string;
 }
 
 type Person = User | Admin;
 
+// type Callback<T> = (callback: (response: ApiResponse<T>) => void) => void;
+// type CallbackApi = Record<string, (callback: (response: ApiResponse<any>) => void) => void>;
+// type ParameterType<T> = T extends Callback<infer R> ? R : never
+// type Promisified<T, P = ParameterType<T[keyof T]>> = Record<string, () => Promise<P>>
+
+type Callback<T> = (callback: (response: ApiResponse<T>) => void) => void;
+type CallbackApi = Record<string, Callback<any>>;
+type ParameterType<T> = T extends Callback<infer R> ? R : never;
+type Promisified<T> = {
+	[K in keyof T]: () => Promise<ParameterType<T[K]>>
+};
+
 const admins: Admin[] = [
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+	{type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator'},
+	{type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver'},
 ];
 
 const users: User[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' }
+	{type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep'},
+	{type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut'},
 ];
 
 export type ApiResponse<T> = (
-    {
-        status: 'success';
-        data: T;
-    } |
-    {
-        status: 'error';
-        error: string;
-    }
-);
+	{
+		status: 'success';
+		data: T;
+	} |
+	{
+		status: 'error';
+		error: string;
+	}
+	);
 
-export function promisify(arg: unknown): unknown {
-    return null;
+export function promisify<T>(arg: Callback<T>): () => Promise<T> {
+	return () => new Promise((resolve, reject) => {
+		arg((response) => {
+			if (response.status === 'success') {
+				resolve(response.data);
+			} else {
+				reject(response.error);
+			}
+		});
+	});
 }
 
 const oldApi = {
-    requestAdmins(callback: (response: ApiResponse<Admin[]>) => void) {
-        callback({
-            status: 'success',
-            data: admins
-        });
-    },
-    requestUsers(callback: (response: ApiResponse<User[]>) => void) {
-        callback({
-            status: 'success',
-            data: users
-        });
-    },
-    requestCurrentServerTime(callback: (response: ApiResponse<number>) => void) {
-        callback({
-            status: 'success',
-            data: Date.now()
-        });
-    },
-    requestCoffeeMachineQueueLength(callback: (response: ApiResponse<number>) => void) {
-        callback({
-            status: 'error',
-            error: 'Numeric value has exceeded Number.MAX_SAFE_INTEGER.'
-        });
-    }
+	requestAdmins(callback: (response: ApiResponse<Admin[]>) => void) {
+		callback({
+			status: 'success',
+			data: admins,
+		});
+	},
+	requestUsers(callback: (response: ApiResponse<User[]>) => void) {
+		callback({
+			status: 'success',
+			data: users,
+		});
+	},
+	requestCurrentServerTime(callback: (response: ApiResponse<number>) => void) {
+		callback({
+			status: 'success',
+			data: Date.now(),
+		});
+	},
+	requestCoffeeMachineQueueLength(callback: (response: ApiResponse<number>) => void) {
+		callback({
+			status: 'error',
+			error: 'Numeric value has exceeded Number.MAX_SAFE_INTEGER.',
+		});
+	},
 };
 
-export const api = {
-    requestAdmins: promisify(oldApi.requestAdmins),
-    requestUsers: promisify(oldApi.requestUsers),
-    requestCurrentServerTime: promisify(oldApi.requestCurrentServerTime),
-    requestCoffeeMachineQueueLength: promisify(oldApi.requestCoffeeMachineQueueLength)
-};
+export const api = promisifyAll(oldApi);
+
+export function promisifyAll(callbackBasedApi: CallbackApi) {
+	const result = {} as Promisified<CallbackApi>;
+
+	for (const key in callbackBasedApi) {
+		result[key] = promisify(callbackBasedApi[key]);
+	}
+
+	return result;
+}
 
 function logPerson(person: Person) {
-    console.log(
-        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
-    );
+	console.log(
+		` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`,
+	);
 }
 
 async function startTheApp() {
-    console.log('Admins:');
-    (await api.requestAdmins()).forEach(logPerson);
-    console.log();
+	console.log('Admins:');
+	(await api.requestAdmins()).forEach(logPerson);
+	console.log();
 
-    console.log('Users:');
-    (await api.requestUsers()).forEach(logPerson);
-    console.log();
+	console.log('Users:');
+	(await api.requestUsers()).forEach(logPerson);
+	console.log();
 
-    console.log('Server time:');
-    console.log(`   ${new Date(await api.requestCurrentServerTime()).toLocaleString()}`);
-    console.log();
+	console.log('Server time:');
+	console.log(`   ${new Date(await api.requestCurrentServerTime()).toLocaleString()}`);
+	console.log();
 
-    console.log('Coffee machine queue length:');
-    console.log(`   ${await api.requestCoffeeMachineQueueLength()}`);
+	console.log('Coffee machine queue length:');
+	console.log(`   ${await api.requestCoffeeMachineQueueLength()}`);
 }
 
 startTheApp().then(
-    () => {
-        console.log('Success!');
-    },
-    (e: Error) => {
-        console.log(`Error: "${e.message}", but it's fine, sometimes errors are inevitable.`);
-    }
+	() => {
+		console.log('Success!');
+	},
+	(e: Error) => {
+		console.log(`Error: "${e.message}", but it's fine, sometimes errors are inevitable.`);
+	},
 );
 
 // In case you are stuck:
